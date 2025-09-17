85fb88d9bb4fbf333cb241135faea30f02b802a5
WsY-Younger <wsy010419@gmail.com>
2025-09-17 22:11:08 +0800

    chore: add gh-pages workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -13,4 +17,5 @@ export default defineConfig({
     port: 5173,
     open: true,
   },
+  base: "/personal-website/",
 });
